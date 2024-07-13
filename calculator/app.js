4da5d477afc1217ccdec71a8045689e81d8f2528
Naseeb-ullah356 <160338963+Naseeb-ullah356@users.noreply.github.com>
2024-07-13 20:16:36 +0500

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,351 @@
+   // alert("hello")
+// alert ('naseeb khan' )
+// alert ("khan ")
+// var fullName = 'naseeb';
+//  alert(fullName)
+//  alert(fullName)
+//   alert(fullName)
+//   alert(fullName)
+// var fullName = 'naseeb';
+// alert(fullName);
+// var fullName = 'khan';
+// alert(fullName);
+
+// var num = 10; 
+// alert(num+10);
+// var num = 20;76`8
+// alert( num+'10'+5);  
+
+// var 1st = 'naseeb';
+// alert (1st);
+// var  alert = 'naseeb '
+// console.log (alert);
+
+// var num = 10;
+// console.log(num%3)
+// var fullName = "naseb\nkhan"
+// alert(fullName)
+// var num = 10;
+// num--; 
+// num = num +1 
+// console.log(++num)
+
+// function func1() {
+//     console.log("function1");
+// }
+// function func2() {
+//     console.log("function2");
+// }
+// function playall() {
+//     func1();
+//     func2();
+
+// } playall();
+
+
+
+// function calculatediscount(price){ 
+//     var price = 5000;
+//     var discount= 50;
+//     var total= price - discount;
+//     console.log(total);
+
+// }
+// calculatediscount();
+
+// var amount = [1, 231, 45, 612, 345, 123, 4, 654, 21];
+
+// function sumArray(arr) {
+//     var sum = 0;
+
+//     for (let num of arr) {
+//         if (typeof num === 'number') {
+//             sum += num;
+//         }
+//     }
+
+//     return sum;
+// }
+
+
+// var totalSum = sumArray(amount);
+// console.log(totalSum);
+
+
+
+// var  arr = [ 12, 45 , 78, ]
+
+
+
+
+
+
+
+
+
+// var firstName=" naseeb"
+// var lastName = "khan"
+// console.log(firstName +lastName)
+
+
+
+
+// var firstName=" naseeb"
+// var lastName = "khan"
+// console.log(firstName  + " "+lastName)
+
+
+
+
+
+// var num = '2';
+// var num2 = '2';
+// console.log(num + num2)
+
+
+
+// var num = 2;
+// var num2 ="2";
+// console.log( 2+ num + num2);
+
+// prompts
+
+// var userName = prompt();
+
+// // var userName = prompt();
+// console.log(userName)
+
+
+// var num1 = +prompt();
+
+
+// var num2= +prompt();
+
+// console.log(num1+num2)
+
+// label
+// var userName = prompt("enter your username")
+
+
+// default value +label
+
+// var userName = prompt("enter your username",'naseeb khan ' )
+
+
+
+// var city = prompt("enter your city name ")
+
+
+//  if ( city == "karachi") {
+//     console.log (" baish hu rahi hai")
+//  }
+
+// var age = + prompt("enter your age")
+// if (age>=20) {
+//     console.log('egligible')
+// } else  {
+//     console.log(' not egligible')
+// }
+
+// var num1=  prompt();
+// var num2 = prompt();
+
+// var opt =prompt();
+
+// var totalCost =  1+(3*4);
+// console.log("totalCost==>", totalCost);
+
+
+// var message = 'helllo world'
+// console.log(typeof message )
+
+
+//   var userName = "naseeb "
+// console.log("your name is " + userName +" thanks  
+
+
+
+// var  gender = prompt ( "enter your gender", "Male")
+// if(gender == "Male"){
+//     alert ("hello sir")
+// }
+// if (gender === "Female"){
+//     alert ("hello sister")
+// }
+
+
+// var  gender = prompt ( "enter your gender", "Male")
+// var message =" not match"
+// if(gender == "Male"){
+//     message =  ("hello sir");
+// }
+// if (gender === "Female"){
+//     message = ("hello sister")
+// }
+// alert (message)
+
+// var gender = prompt("enter your  gender" ,'Male')
+// var message = "not match"
+//     if (gender== "Male"){
+//         message = ("hello sir ")
+//     }
+//     if (gender== "female"){
+//         message = ("hello sister")
+//     }
+
+// alert (messag
+//  if ( "a"==="a"){  
+//        console.log("hello")
+//  }
+
+// var userName = prompt ("enter your name ");
+// if (userName){
+//     console.log(userName)
+// }else{
+//     alert ("please enter name")
+// }
+
+
+// var userName = prompt("enter your name")
+//  if (userName){
+//     console.log(userName)
+//  }else{
+//     alert("please enter your name")
+//  }
+//  var userName = prompt("enter your name")
+//  if (userName){
+//     console.log (userName)
+//  }else{
+//     alert("please enter your name")
+//  }
+
+// var skills = prompt("enter your skills");
+// if (skills=== "html"){
+//     console.log("10000")}
+//   else if(skills==="css"){
+//     console.log("20000")
+// }
+// else{
+//     console.log("0")
+// }
+
+// var skills = prompt("enter your skills");
+// var exp = prompt("enter your exp");
+// var city = prompt("enter your city")
+
+// if ((skills ==="javascript"||exp > 2) &&city==="karachi" ){
+// console.log("hired")
+// }
+// Array
+
+// var arr =["naseeb,janzaib, khan,fazal, irfan"];
+// console.log(arr)
+// function abc(elem) {
+//     console.log
+// }
+// }
+// // function  abc (elem){
+// //     console.log("inputBlured") ;
+// //      elem.style.backgroundColor= "white"
+//   function flip (elem,dimond) {
+//    if (Count<3) {
+//     if (dimond == "yes") {
+//         elem.src=img1;
+        
+//     }else{
+//         elem.src=img2;
+//     }
+// count++;
+    
+//    } else{}
+
+
+//  var arr = ["naseeb","khan","abdullah","moosa"];
+//  console.log(arr)
+// indexing
+// var arr = ["naseeb","irfan","abdullah","qasim","zeeshan"];
+// console.log(arr)
+
+// var arr = ["naseeb","khan","abdullah","moosa"];
+// console.log(arr[3])
+// var arr = ["naseeb","irfan","abdullah","qasim","zeeshan"];
+// console.log(arr[3])
+// var arr = ["naseeb","irfan","abdullah","qasim","zeeshan"];
+// console.log(arr[0])
+// dot length
+// var arr = ["naseeb","khan","abdullah","moosa"];
+// arr [4] = "mustafa"
+// console.log(arr)
+// var arr = ["naseeb","irfan","abdullah","qasim","zeeshan"];
+// arr [4] = "khan"
+// console.log(arr)
+// var arr = ["mustafa","moosa","momina","fatima"]
+// arr [3] = "fareed"
+// console.log(arr)
+
+// var arr =[ "naseeb","khanhhhhh"]
+// var arr= ["naseeb","khan","abdullah","moosa"];
+
+// arr="kamran"
+//  console.log(arr[3])
+// pop and push method in java script
+// pop meaning to remove one element from last
+// var arr = ["naseeb","khan","abdullah","moosa"];
+// arr.pop(3)
+
+// console.log(arr)
+// push method in java script
+// var arr = ["naseeb","khan","abdullah","moosa"];
+// arr.push("mustafa","zeeshan")
+
+// console.log(arr)
+// var arr = ["naseeb","khan","abdullah","moosa"];
+// arr.unshift("mustafa","zeeshan")
+
+// console.log(arr)
+// var arr = ["naseeb","khan","abdullah","moosa"];
+// arr.unshift("mustafa","zeeshan")
+// arr.shift()
+// console.log(arr)
+// splice
+// var student = ["ali","ahmed","zain",];
+
+// student.splice(1,3);
+// console.log(student);
+
+//  var student = ["ali","ahmed","zain",];
+
+// student.splice(1,1, " hassan", "5646");
+//  console.log(student);
+// delete kuch bhi nhi krna
+// var student = ["ahmed","ali","zain",];
+
+// student.splice(1,0, "hassan","raza");
+//  console.log(student);
+
+// sclice
+
+// var student = ["ali","ahmed","zain", "hassan"];
+
+
+//  console.log(student.shiftlice(0,2));
+//  console.log(student.shiftlice(2));
+var n1 = document.getElementById("num1");
+var n2 = document.getElementById("num2");
+var res = document.getElementById("result");
+ document.getElementById("add").addEventListener("click" , function() {
+res. value = 
+parseInt(n1.value)+parseInt(n2.value);
+  });
+  document.getElementById("sub").addEventListener("click" , function() {
+   res.value = 
+      parseInt(n1.value)- parseInt(n2.value);
+     });
+     document.getElementById("mul").addEventListener("click" , function() {
+      res.value = 
+         parseInt(n1.value)* parseInt(n2.value);
+        });
+        document.getElementById("div").addEventListener("click" , function() {
+         res.value = 
+            parseInt(n1.value)/ parseInt(n2.value);
+           });
